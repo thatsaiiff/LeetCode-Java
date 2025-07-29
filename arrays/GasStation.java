bcc9a593d7dc44825ae392f9a63c2dc8fa1ad266
Saif Alam <saif@Mac.dlink>
2025-07-30 00:51:14 +0530

    Solved Gas Station problem using greedy approach

---
+++ arrays/GasStation.java
@@ -0,0 +1,20 @@
+public class GasStation {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int totalTank=0;
+        int currentTank=0;
+        int startIndex=0;
+
+        for (int i = 0; i < gas.length; i++) {
+            int gain = gas[i]-cost[i];
+            totalTank+=gain;
+            currentTank+=gain;
+
+            if (currentTank<0){
+                startIndex=i+1;
+                currentTank=0;
+            }
+
+        }
+        return totalTank>=0 ? startIndex : -1;
+    }
+}
